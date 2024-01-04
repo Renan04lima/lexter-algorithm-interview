9556c789c06e16366d6929f65f9deb33cef3a3b0
renan04lima <renan04lima@outlook.com>
2024-01-04 18:44:59 -0300

    test: ensure an entry must be a child of another if the beginning of the path of both is the same, that is, the \'root/path\' entry is a child of the \'root\' entry

--- tests/nested-path-assembler.spec.ts
+++ tests/nested-path-assembler.spec.ts
@@ -2,8 +2,17 @@ import { Input } from "@/data/input-default"
 
 class NestedPathsAssembler {
     execute(inputList: Input[]): any[] {
-        const output = inputList
-            .map(i => ({ entryId: Number(i.entryId) }))
+        type Output = {
+            entryId: number
+            path: string[]
+            children: Output[]
+        }
+        const output: Output[] = inputList
+            .map(i => ({
+                entryId: Number(i.entryId),
+                path: i.path,
+                children: []
+            }))
             .sort((a, b) => {
                 if (a.entryId > b.entryId) {
                     return 1
@@ -14,6 +23,19 @@ class NestedPathsAssembler {
                 }
 
             })
+
+        for (let i = 0; i < output.length; i++) {
+            for (let j = i + 1; j < output.length; j++) {
+                if (output[i].path.includes(output[j].path[0])) {
+                    const copiedObject: Output = JSON.parse(JSON.stringify(output[j]));
+
+                    output[i].children.push(copiedObject);
+
+                    output.splice(j, 1);
+                    j--;
+                }
+            }
+        }
 
         return output
     }
@@ -29,12 +51,42 @@ describe('NestedPathsAssembler', () => {
         ]
         const output = sut.execute(input)
 
-        expect(output.map(o => o.entryId)).toEqual([
+        const a = output.map(o => o.entryId)
+        console.log(a)
+        console.log(output)
+        expect(a).toEqual([
             1, 2, 3
         ])
     })
 
-    test.todo('An entry must be a child of another if the beginning of the path of both is the same, that is, the \'root/path\' entry is a child of the \'root\' entry')
+    test('An entry must be a child of another if the beginning of the path of both is the same, that is, the \'root/path\' entry is a child of the \'root\' entry', () => {
+        const sut = new NestedPathsAssembler()
+        const input = [
+            { entryId: "1", path: ["root1"] },
+            { entryId: "2", path: ["root1", "path2"] },
+            { entryId: "3", path: ["root1", "path2", "path2"] },
+        ]
+        const output = sut.execute(input)
+
+        expect(output).toEqual([
+            {
+                entryId: 1,
+                path: ["root1"],
+                children: [
+                    {
+                        children: [],
+                        entryId: 2,
+                        path: ["root1", "path2"],
+                    },
+                    {
+                        children: [],
+                        entryId: 3,
+                        path: ["root1", "path2", "path2"],
+                    },
+                ],
+            }
+        ])
+    })
     test.todo('The Output fullPath key is a string with all path elements separated by /')
     test.todo('The currentPath key is the current path value. In other words, for the fullPath entry roo/path, your currentPath is path')
 })
