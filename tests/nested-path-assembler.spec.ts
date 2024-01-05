4f4b1238abd83030e85b47b17ec1ed97331d9af9
renan04lima <renan04lima@outlook.com>
2024-01-05 14:56:21 -0300

    test: ensure the output has an ordered fullPath and make adding children recursive

--- tests/nested-path-assembler.spec.ts
+++ tests/nested-path-assembler.spec.ts
@@ -1,18 +1,23 @@
 import { Input } from "@/data/input-default"
 
+type Output = {
+    entryId: number
+    fullPath: string
+    children: Output[]
+}
 class NestedPathsAssembler {
     execute(inputList: Input[]): any[] {
-        type Output = {
-            entryId: number
-            path: string[]
-            children: Output[]
-        }
         const output: Output[] = inputList
-            .map(i => ({
-                entryId: Number(i.entryId),
-                path: i.path,
-                children: []
-            }))
+            .map(i => {
+                const [root, ...paths] = i.path;
+                const fullPath = `${root}${paths.length > 0 ? `/${paths.sort().join('/')}` : ''}`;
+
+                return {
+                    entryId: Number(i.entryId),
+                    fullPath,
+                    children: [],
+                }
+            })
             .sort((a, b) => {
                 if (a.entryId > b.entryId) {
                     return 1
@@ -24,9 +29,15 @@ class NestedPathsAssembler {
 
             })
 
+        this.addChildren(output)
+
+        return output
+    }
+
+    private addChildren(output: Output[]) {
         for (let i = 0; i < output.length; i++) {
             for (let j = i + 1; j < output.length; j++) {
-                if (output[i].path.includes(output[j].path[0])) {
+                if (output[i].fullPath.split('/').includes(output[j].fullPath.split('/')[0])) {
                     const copiedObject: Output = JSON.parse(JSON.stringify(output[j]));
 
                     output[i].children.push(copiedObject);
@@ -35,9 +46,10 @@ class NestedPathsAssembler {
                     j--;
                 }
             }
+            if (output[i].children.length > 0) {
+                this.addChildren(output[i].children)
+            }
         }
-
-        return output
     }
 }
 
@@ -51,11 +63,24 @@ describe('NestedPathsAssembler', () => {
         ]
         const output = sut.execute(input)
 
-        const a = output.map(o => o.entryId)
-        console.log(a)
-        console.log(output)
-        expect(a).toEqual([
+        expect(output.map(o => o.entryId)).toEqual([
             1, 2, 3
+        ])
+    })
+
+    test('The Output fullPath key is a string with all path elements separated by /', () => {
+        const sut = new NestedPathsAssembler()
+        const input = [
+            { entryId: "3", path: ["root3", "path2"] },
+            { entryId: "1", path: ["root1", "path3", "path2"] },
+            { entryId: "2", path: ["root2", "path1"] },
+        ]
+        const output = sut.execute(input)
+
+        expect(output.map(o => o.fullPath)).toEqual([
+            "root1/path2/path3",
+            "root2/path1",
+            "root3/path2",
         ])
     })
 
@@ -68,25 +93,25 @@ describe('NestedPathsAssembler', () => {
         ]
         const output = sut.execute(input)
 
-        expect(output).toEqual([
+        expect(output).toStrictEqual([
             {
                 entryId: 1,
-                path: ["root1"],
+                fullPath: "root1",
                 children: [
                     {
-                        children: [],
                         entryId: 2,
-                        path: ["root1", "path2"],
-                    },
-                    {
-                        children: [],
-                        entryId: 3,
-                        path: ["root1", "path2", "path2"],
+                        fullPath: "root1/path2",
+                        children: [
+                            {
+                                entryId: 3,
+                                fullPath: "root1/path2/path2",
+                                children: [],
+                            },
+                        ],
                     },
                 ],
             }
         ])
     })
-    test.todo('The Output fullPath key is a string with all path elements separated by /')
     test.todo('The currentPath key is the current path value. In other words, for the fullPath entry roo/path, your currentPath is path')
 })
