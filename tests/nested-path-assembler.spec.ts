b83aa89596f92d4aca7da0880ad7ff6ba9632c39
renan04lima <renan04lima@outlook.com>
2024-01-09 11:54:17 -0300

    refactor: simplify tests

--- tests/nested-path-assembler.spec.ts
+++ tests/nested-path-assembler.spec.ts
@@ -1,13 +1,19 @@
 import { NestedPathsAssembler } from "@/app/api/nested-paths/nested-path-assembler"
 
 describe('NestedPathsAssembler', () => {
+    let sut: NestedPathsAssembler
+
+    beforeEach(() => {
+        sut = new NestedPathsAssembler()
+    })
+
     test('All output list levels must be in ascending order by entryId', () => {
-        const sut = new NestedPathsAssembler()
         const input = [
             { entryId: "3", path: ["root3"] },
             { entryId: "1", path: ["root1"] },
             { entryId: "2", path: ["root2"] },
         ]
+
         const output = sut.execute(input)
 
         expect(output.map(o => o.entryId)).toEqual([
@@ -15,65 +21,16 @@ describe('NestedPathsAssembler', () => {
         ])
     })
 
-    test('The Output fullPath key is a string with all path elements separated by /', () => {
-        const sut = new NestedPathsAssembler()
-        const input = [
-            { entryId: "1", path: ["root1"] },
-            { entryId: "3", path: ["root1", "path2"] },
-            { entryId: "2", path: ["root1", "path3", "path2"] },
-            { entryId: "4", path: ["root2"] },
-        ]
-        const output = sut.execute(input)
-
-        expect(output.map(o => o.fullPath)).toEqual([
-            "root1",
-            "root2",
-        ])
-    })
-
     test('An entry must be a child of another if the beginning of the path of both is the same, that is, the \'root/path\' entry is a child of the \'root\' entry', () => {
-        const sut = new NestedPathsAssembler()
         const input = [
             { entryId: "1", path: ["root1"] },
+            { entryId: "5", path: ["root2"] },
             { entryId: "2", path: ["root1", "path2"] },
             { entryId: "3", path: ["root1", "path2", "path2"] },
             { entryId: "4", path: ["root1", "path3", "path2"] },
+            { entryId: "6", path: ["root2", "path1"] },
         ]
-        const output = sut.execute(input)
 
-        expect(output).toMatchObject([
-            {
-                entryId: 1,
-                fullPath: "root1",
-                children: [
-                    {
-                        entryId: 2,
-                        fullPath: "root1/path2",
-                        children: [
-                            {
-                                entryId: 3,
-                                fullPath: "root1/path2/path2",
-                                children: [],
-                            },
-                        ],
-                    },
-                    {
-                        entryId: 4,
-                        fullPath: "root1/path3/path2",
-                        children: [],
-                    },
-                ],
-            }
-        ])
-    })
-
-    test('The currentPath key is the current path value. In other words, for the fullPath entry roo/path, your currentPath is path', () => {
-        const sut = new NestedPathsAssembler()
-        const input = [
-            { entryId: "1", path: ["root1"] },
-            { entryId: "2", path: ["root1", "path2"] },
-            { entryId: "3", path: ["root1", "path2", "path2"] },
-        ]
         const output = sut.execute(input)
 
         expect(output).toStrictEqual([
@@ -89,14 +46,33 @@ describe('NestedPathsAssembler', () => {
                         children: [
                             {
                                 entryId: 3,
-                                currentPath: "path2",
                                 fullPath: "root1/path2/path2",
+                                currentPath: "path2",
                                 children: [],
                             },
                         ],
                     },
+                    {
+                        entryId: 4,
+                        fullPath: "root1/path3/path2",
+                        currentPath: "path2",
+                        children: [],
+                    },
                 ],
-            }
+            },
+            {
+                entryId: 5,
+                fullPath: "root2",
+                currentPath: "root2",
+                children: [
+                    {
+                        entryId: 6,
+                        fullPath: "root2/path1",
+                        currentPath: "path1",
+                        children: [],
+                    },
+                ],
+            },
         ])
     })
 
@@ -106,8 +82,6 @@ describe('NestedPathsAssembler', () => {
             { entryId: "2", path: ["root1", "path2"] },
             { entryId: "3", path: ["root1", "path2", "path2"] },
         ]
-
-        const sut = new NestedPathsAssembler()
 
         expect(() => sut.execute(input)).toThrow(new Error('Missing root path: root1'))
     })
