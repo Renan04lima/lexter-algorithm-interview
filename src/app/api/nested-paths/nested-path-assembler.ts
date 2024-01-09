c2c9b9a5a6d14a5341c56acc40522f5c3f0fbd3a
renan04lima <renan04lima@outlook.com>
2024-01-09 11:43:18 -0300

    feat: throw a error if is missing a root path

--- src/app/api/nested-paths/nested-path-assembler.ts
+++ src/app/api/nested-paths/nested-path-assembler.ts
@@ -1,13 +1,8 @@
-import { Input } from "@/data/input-default"
+import { Input } from "@/types/input"
+import { Output } from "@/types/output"
 
-type Output = {
-    entryId: number
-    fullPath: string
-    children: Output[]
-    currentPath: string
-}
 export class NestedPathsAssembler {
-    execute(inputList: Input[]): any[] {
+    execute(inputList: Input[]): Output[] {
         const output: Output[] = inputList
             .map(i => {
                 return {
@@ -30,27 +25,36 @@ export class NestedPathsAssembler {
 
         this.addChildren(output)
 
+        output.forEach(item => {
+            const rootPath = item.fullPath.split('/')[0];
+
+            if (!output.some(otherItem => otherItem.fullPath === rootPath)) {
+                throw new Error(`Missing root path: ${rootPath}`);
+            }
+        });
+
         return output
     }
 
     private isChildren(parent: Output, potentialChild: Output): boolean {
-        return potentialChild.fullPath.startsWith(parent.fullPath + '/');
+        return potentialChild.fullPath.startsWith(parent.fullPath + '/')
     }
 
     private addChildren(output: Output[]) {
         for (let i = 0; i < output.length; i++) {
             output[i].currentPath = output[i].fullPath.split('/').pop() || '';
+            
             for (let j = i + 1; j < output.length; j++) {
-                const isChildren = this.isChildren(output[i], output[j]);
-                if (isChildren) {
-                    const copiedObject: Output = JSON.parse(JSON.stringify(output[j]));
+                if (this.isChildren(output[i], output[j])) {
+                    const copiedObject: Output = JSON.parse(JSON.stringify(output[j]))
 
-                    output[i].children.push(copiedObject);
+                    output[i].children.push(copiedObject)
 
-                    output.splice(j, 1);
+                    output.splice(j, 1)
                     j--;
                 }
             }
+
             if (output[i].children.length > 0) {
                 this.addChildren(output[i].children)
             }
